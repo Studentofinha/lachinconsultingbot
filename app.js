f8d0347f8480204c53ac48a240b3417b9058baae
Tozaboev Sardorbek <studentofinha@gmail.com>
2023-01-19 13:19:15 +0500

    added

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-const express=require("express");
+const {Telegraf} = require("telegraf");
+const bot=new Telegraf(`5974755223:AAHXtZMqGxjnOpxyt-qvCy3_60087652eMM`);
+
+const CronJob = require('cron').CronJob;
+
+
+bot.start((ctx)=>{
+    
+    const job = new CronJob('0/15 * * * * *',function() {
+        ctx.replyWithDocument({source:'./file/frontend.pdf'})
+        },
+        null,
+        true
+    );
+    job.start()
+})
+
+bot.launch();
